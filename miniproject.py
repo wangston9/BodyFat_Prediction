064890b1e83c9d1500f08992f9e2ff11d0d582e7
Winston Wang <95153031+wangston9@users.noreply.github.com>
2023-08-28 15:47:03 +0800

    Update miniproject.py

--- miniproject.py
+++ miniproject.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd 
-import matplotlib.pyplot as plt 
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
